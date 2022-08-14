b03ed1176f69c74eb7fd4dae5a9ac3e530769a96
Grady Thompson <gradyt513@gmail.com>
2022-08-13 18:17:52 -0700

    Add humans.txt and more metadata

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
 const htmlmin = require("html-minifier");
 
+const date = new Date();
+
 module.exports = function (eleventyConfig) {
+
+	eleventyConfig.addGlobalData("currentYear", function () {
+		return date.getFullYear();
+	});
+
+	eleventyConfig.addGlobalData("currentDate", function () {
+		return date.toISOString().substring(0, 10).replaceAll("-", "/");
+	});
+
 	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
 		if (outputPath && outputPath.endsWith(".html")) {
 			let minified = htmlmin.minify(content, {
@@ -9,8 +20,7 @@ module.exports = function (eleventyConfig) {
 				collapseWhitespace: true
 			});
 			return minified;
-		}
-
+		};
 		return content;
 	});
 
@@ -28,5 +38,5 @@ module.exports = function (eleventyConfig) {
 		dir: {
 			input: "src"
 		}
-	}
-}
+	};
+};
---
+++ src/humans.md
@@ -0,0 +1,21 @@
+---
+templateEngineOverride: njk
+permalink: "humans.txt"
+---
+# humans.txt
+
+[The humans responsible & technology colophon](https://humanstxt.org/)
+
+_This file contains valid markdown._
+
+## Team
+
+**{{ pkg.author.name }}**
+Twitter: {{ site.author.twitter }}
+
+## Site
+
+Last update: {{ currentDate }}
+Standards: HTML, CSS
+Components: Eleventy
+Software: Visual Studio Code, Git, GitHub, Microsoft Edge, Node.js, npm
