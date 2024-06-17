908fed429de8347a73f8cb21f994ac5c68d32efa
Grady Thompson <gradyt513@gmail.com>
2024-06-16 21:26:49 -0700

    fix: switch to html-minifier-terser

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,25 +1,30 @@
-import htmlmin from "html-minifier";
+import htmlmin from "html-minifier-terser";
 import CleanCSS from "clean-css";
 
 export default async function(eleventyConfig) {
-	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-		if (outputPath && outputPath.endsWith(".html")) {
-			let minified = htmlmin.minify(content, {
-				useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true
-			});
-			return minified;
-		};
-		return content;
-	});
-
-	eleventyConfig.addTransform("cssmin", function (code, outputPath) {
-		if (outputPath && outputPath.endsWith(".css")) {
-			return new CleanCSS({}).minify(code).styles;
-		};
-		return code;
-	});
+	if (process.env.ELEVENTY_RUN_MODE === "build") {
+		eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+			if (outputPath && outputPath.endsWith(".html")) {
+				let minified = htmlmin.minify(content, {
+					collapseBooleanAttributes: true,
+					collapseWhitespace: true,
+					removeComments: true,
+					sortAttributes: true
+					// old
+				});
+				return minified;
+			};
+			return content;
+		});
+	
+		eleventyConfig.addTransform("cssmin", function (code, outputPath) {
+			if (outputPath && outputPath.endsWith(".css")) {
+				return new CleanCSS({}).minify(code).styles;
+			};
+			return code;
+		});
+	}
+	
 
 	eleventyConfig.addPassthroughCopy("src/_headers");
 	eleventyConfig.addPassthroughCopy("src/robots.txt");
